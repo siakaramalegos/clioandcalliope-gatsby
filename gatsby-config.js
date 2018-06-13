ea91629c765e9ae09384ba581ad41fea3ee9ac15
Sia <siakaramalegos@gmail.com>
2018-06-13 12:14:08 -0500

    Adds styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Clio + Calliope App Development',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+  ],
 }
